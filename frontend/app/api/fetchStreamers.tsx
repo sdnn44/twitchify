f9c595d280794f22590e341f0db41664984e1594
sdnn44 <slidennnq@gmail.com>
2024-02-23 20:51:12 +0100

    Corrected typo

--- frontend/app/api/fetchStreamers.tsx
+++ frontend/app/api/fetchStreamers.tsx
@@ -1,10 +1,11 @@
 "use server";
 
 import axios from "axios";
+const BASE_URL = "https://twitchify-server.vercel.app/";
 
 export async function fetchStreamerByName(name: string) {
     try {
-        const response = await axios.get(`https://twitchify-server.vercel.app/${name}`);
+        const response = await axios.get(`${BASE_URL}/${name}`);
         // const data = await response.json();
         console.log('Clips received:', response.data);
         return response.data;
